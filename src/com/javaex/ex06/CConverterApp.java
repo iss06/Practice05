82580f8d277561ed3ca3684df590be31b16954d4
하이미디어학원 <@DESKTOP-LJU20GE>
2024-03-26 18:05:05 +0900

    0326

--- src/com/javaex/ex06/CConverterApp.java
+++ src/com/javaex/ex06/CConverterApp.java
@@ -8,13 +8,15 @@ public class CConverterApp {
         double dollar;
         double won;
         
-        CConverter.setRate(1118.70);
+        CConverter.setRate(1341.30);	//	환율 세팅
         
         //100만원을 달러로 출력하기
-
+        dollar = CConverter.toDollar(1_000_000);
+        System.out.println("백만원은 " + dollar + "달러입니다.");
         
         //100달려를 원으로 출력하기
-        
+        won = CConverter.toKWR(100);
+        System.out.println("백달러는 " + won + "원입니다.");
         
     }
 
--- src/com/javaex/ex08/Account.java
+++ src/com/javaex/ex08/Account.java
@@ -2,11 +2,50 @@ package com.javaex.ex08;
 
 public class Account {
 
-    private String accountNo;
-    private int balance;
-    
-    //생성자 작성
-    
-    //필요한 메소드 작성
+	private String accountNo;
+	private int balance;
+
+	// 생성자 작성
+	public Account(String accountNo, int balance) {
+		this.accountNo = accountNo;
+		this.balance = balance;
+	}
+
+	public String getAccountNo() {
+		return accountNo;
+	}
+
+	public void setAccountNo(String accountNo) {
+		this.accountNo = accountNo;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	// 필요한 메소드 작성
+	public void deposit (int amount) {
+		if (amount > 0) {
+			balance += amount;
+			System.out.println(amount);
+		}
+	}
+	
+	public void withdraw (int amount) {
+		if (amount >0 && amount <= balance) {
+			System.out.println(amount);
+		}
+	}
+	
+	public void showBalance (int amount) {
+		if (amount > 0 && amount == balance) {
+			System.out.println(amount);
+		}
+	}
+	
 
 }
